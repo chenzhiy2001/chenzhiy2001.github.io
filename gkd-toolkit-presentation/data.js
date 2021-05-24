1f3c72ba9f92ad2ce2175b8563424965ed399d43
chenzhiy2001 <30455060+chenzhiy2001@users.noreply.github.com>
2021-05-24 21:14:26 +0800

    added gkd-toolkit for presentation

---
+++ gkd-toolkit-presentation/data.js
@@ -0,0 +1,37 @@
+myData={
+  "tree": [
+    {
+      "nodeId": 0,
+      "title": "test",
+      "posX": "44px",
+      "posY": "72px",
+      "child": [
+        1,
+        2
+      ],
+      "father": []
+    },
+    {
+      "nodeId": 1,
+      "title": "",
+      "posX": "194px",
+      "posY": "72px",
+      "child": [],
+      "father": [
+        0
+      ]
+    },
+    {
+      "nodeId": 2,
+      "title": "",
+      "posX": "194px",
+      "posY": "172px",
+      "child": [],
+      "father": [
+        [
+          0
+        ]
+      ]
+    }
+  ]
+}
